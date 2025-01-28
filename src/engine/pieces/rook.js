3ab307117a0a0de4de4be0c55cf57d0bb89133a2
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-01-28 13:38:18 +0000

    Available moves for rook (laterally and others)

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,29 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const moves = [];
+        let location = board.findPiece(this);
+        let currRow = location.row;
+        let currCol = location.col;
+        if (this.player === Player.WHITE) {
+
+             //Horizontal
+             for(let column = 0; column < 8; column++ ){
+                if(column !== currCol){ 
+                  moves.push(Square.at(location.row , column));
+                }  
+              } 
+            
+            //Vertical  
+            for(let row = 0; row < 8; row++ ){
+                if(row !== currRow){ 
+                  moves.push(Square.at(row , location.col));
+                }  
+              }
+        }
+        else {
+
+        }     
+        return moves;
     }
 }
