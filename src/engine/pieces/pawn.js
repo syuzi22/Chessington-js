37751f8b05b93004294eb5457234f0490c28d290
Siuzanna Abkadyrova <siuzanna.abkadyrova@softwire.com>
2025-01-28 14:25:05 +0000

    Add a check of blocking pieces for pawns

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -7,23 +7,46 @@ export default class Pawn extends Piece {
         super(player);
     }
 
+    checkIfSquareIsEmpty(move, board) {
+        return board.getPiece(move) === undefined;
+    }
+
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         let moves = [];
 
         if (this.player === Player.WHITE) {
             const isFirstMove = location.row === 1;
-            moves.push(Square.at(location.row + 1, location.col));
+            const move = Square.at(location.row + 1, location.col);
+            const isSquareEmpty = this.checkIfSquareIsEmpty(move, board);
 
-            if (isFirstMove) {
-                moves.push(Square.at(location.row + 2, location.col));
+            if (isSquareEmpty) {
+                moves.push(move);
+            }
+
+            if (isFirstMove && isSquareEmpty) {
+                const nextMove = Square.at(location.row + 2, location.col);
+
+                if (this.checkIfSquareIsEmpty(nextMove, board)) {
+                    moves.push(nextMove);
+                }
+               
             }
         } else {
             const isFirstMove = location.row === 6;
-            moves.push(Square.at(location.row - 1, location.col));
+            const move = Square.at(location.row - 1, location.col);
+            const isSquareEmpty = this.checkIfSquareIsEmpty(move, board);
 
-            if (isFirstMove) {
-                moves.push(Square.at(location.row - 2, location.col));
+            if (isSquareEmpty) {
+                moves.push(move);
+            }
+
+            if (isFirstMove && isSquareEmpty) {
+                const nextMove = Square.at(location.row - 2, location.col);
+                
+                if (this.checkIfSquareIsEmpty(nextMove, board)) {
+                    moves.push(nextMove);
+                }  
             }
         }
 
