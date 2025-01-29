355abb9be195f0d5857bff3073f7ef74cc8c1bb2
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-01-29 16:12:30 +0000

    Added diagonal moves for Bishop

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,11 +1,39 @@
 import Piece from './piece';
+import Player from '../player'
+import {checkIfRowWithinBoard,checkIfColWithinBoard} from '../utils.js';
+import Square from '../square.js';
+
 
 export default class Bishop extends Piece {
     constructor(player) {
         super(player);
     }
 
+
     getAvailableMoves(board) {
-        return new Array(0);
+        const moves = [];
+        let location = board.findPiece(this);
+        let currRow = location.row;
+        let currCol = location.col;
+        let move;
+        for (let i = 1; i < 8; i++) {
+            if (currRow + i < 8 && currCol + i < 8) {
+                move = Square.at(currRow + i, currCol + i);//Bottom -right
+                moves.push(move); 
+            }
+            if (currRow + i < 8 && currCol - i >= 0){
+                move =  Square.at (currRow + i, currCol - i); // Bottom-left
+                moves.push(move); 
+            } 
+            if (currRow - i >= 0 && currCol + i < 8){
+                 move = Square.at (currRow - i, currCol + i); // Top-right
+                 moves.push(move);
+            }     
+            if (currRow - i >= 0 && currCol - i >= 0){
+                move = Square.at (currRow - i, currCol - i); // Top-left
+                moves.push(move);
+            }    
+        }
+        return moves;
     }
 }
--- src/engine/utils.js
+++ src/engine/utils.js
@@ -1,4 +1,29 @@
 import King from "./pieces/king";
+import Player from "./player";
+
 export const checkIfKing = (board,move) => {
     return board.getPiece(move) instanceof King;
+}
+
+export const checkIfRowWithinBoard = (row, player) =>{
+    let topRow = 7;
+    let bottomRow = 0;
+    if((player=== Player.WHITE) && (row <= topRow)){
+        return true;
+    }
+    else if ((player=== Player.BLACK) && (row >= bottomRow)){
+        return true;
+    } 
+    return false;   
+}
+
+export const checkIfColWithinBoard = (col) =>{
+    if(col >=0 && col < 8){
+        return true;
+    }
+    return false;   
+}
+
+export const checkIfSquareIsEmpty = (move, board,nextRow,player) => {
+    return board.getPiece(move) === undefined;
 }
